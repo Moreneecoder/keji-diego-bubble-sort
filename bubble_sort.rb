1c5957ace1ae383fff5b1f04cd062b5a47b312f8
Morenikeji Bello <caeserselassie@gmail.com>
2021-02-10 20:59:33 +0100

    Wrote bubble_sort_by logic with yield

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,3 +10,15 @@ def bubble_sort(array)
 end
 
 p bubble_sort([4, 3, 78, 2, 0, 2])
+
+def bubble_sort_by(array)
+  array.length.times do
+    array.each_index do |idx|
+      diff = yield(array[idx], array[idx + 1]) if idx < array.length - 1
+      array[idx], array[idx + 1] = array[idx + 1], array[idx] if diff.to_i.positive?
+    end
+  end
+  p array
+end
+
+bubble_sort_by(%w[hi hello hey help]) { |left_word, right_word| left_word.length - right_word.length }
